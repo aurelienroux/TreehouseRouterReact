3421d89eeabbd1992bdc115af06a938967921a3d
Aurelien Roux <aureroux13@me.com>
2017-02-18 13:41:25 -0500

    fixed teachers list data

--- src/components/Teachers.js
+++ src/components/Teachers.js
@@ -1,16 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
+import TeacherList from '../data/teachers.js'
 
-class Teachers extends Component {
-  render(){
+const Teachers = () => {
+  let teachers = TeacherList.map((teacher) => {
     return (
-      <div className="main-content">
-        <h2>Teachers</h2>
-        <ul className="group">
-          {/* {teachers} */}
-        </ul>
-      </div>
-    )
-  }
+      <li className="teacher" key={teacher.id} >
+        <img className="teacher-img" src={teacher.img_src} />
+        <h3>{teacher.name}</h3>
+        <p>{teacher.bio}</p>
+      </li>
+    );
+  });
+
+  return (
+    <div className="main-content">
+      <h2>Teachers</h2>
+      <ul className="group">
+        {teachers}
+      </ul>
+    </div>
+  );
 }
+
 
 export default Teachers;
