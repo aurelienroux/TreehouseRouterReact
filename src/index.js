06bdb730792a3fd02f7d19343d3c4e15d933a36a
Aurelien Roux <aureroux13@me.com>
2017-02-18 14:08:13 -0500

    organizing routes in routes.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,15 @@
-//Libs
+// Libs
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router';
 
-//CSS
+// CSS
 import './css/index.css';
 import './css/style.css';
 
-//Components
-import App from './components/App';
-import Home from './components/Home';
-import About from './components/About';
-import Courses from './components/Courses';
-import Teachers from './components/Teachers';
+// Routes
+import routes from './router';
 
-ReactDOM.render((
-  <Router history={hashHistory}>
-    <Route component={App}>
-      <Route path="/" component={Home} />
-      <Route path="about" component={About} />
-      <Route path="teachers" component={Teachers} />
-      <Route path="courses" component={Courses} />
-    </Route>
-  </Router>
-), document.getElementById('root'));
+
+ReactDOM.render(
+  routes,
+ document.getElementById('root'));
